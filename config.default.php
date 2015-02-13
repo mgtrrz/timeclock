1a03899bb2d50bc4fff71a59d1c35b50a6215045
Marcus Gutierrez <markg90@gmail.com>
2015-02-13 14:31:58 -0600

    Changed config files

---
+++ config.default.php
@@ -0,0 +1,16 @@
+<?php
+
+/* Database info */
+define('DB_HOST','localhost');
+define('DB_NAME','');
+define('DB_USER','');
+define('DB_PASS','');
+
+/* Site name */
+define('SITE_NAME','Timeclock');
+
+/* Company name */
+define('COMPANY_NAME', '');
+
+/* Developer/Debug Mode */
+define('DEV_DEBUG', '1');
